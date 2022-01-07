98ca1a136162c7b67c50549109fd0ec1356076b9
David Kim <cjoak1028@gmail.com>
2022-01-06 22:38:00 -0500

    content of popup changes depending on website url

---
+++ background.js
@@ -0,0 +1,16 @@
+chrome.tabs.onActivated.addListener(activeTab => {
+    chrome.tabs.get(activeTab.tabId, (tab) => {
+        console.log(tab.url);
+        if (/^https:\/\/www\.netflix\.com\/browse/.test(tab.url)) {
+            chrome.action.setPopup({
+                popup: 'popup.html',
+                tabId: tab.id
+            });
+        } else {
+            chrome.action.setPopup({
+                popup: 'alert.html',
+                tabId: tab.id
+            });
+        }
+    })
+});
